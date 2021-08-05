82b9c0b23fd40f8d0754118aced31a48bad2f856
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-08-05 09:52:49 +0200

    Initial push

---
+++ src/main/java/com/java/game/ahorcado_Didac_David/App.java
@@ -0,0 +1,13 @@
+package com.java.game.ahorcado_Didac_David;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
